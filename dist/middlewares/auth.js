8d3e8965f04e0088dce618b454337ba609081a15
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-04 16:51:37 +0530

    New upadtion converted to tsc

---
+++ dist/middlewares/auth.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+exports.default = {
+    generateToken: (id) => {
+        const token = jwt.sign({ id }, process.env.JWTSECRET);
+        return token;
+    },
+    verifyToken: (req, res, next) => {
+        try {
+            let token = req.headers.authorization;
+            if (!token) {
+                return res.status(403).json({ errMsg: "Access denied" });
+            }
+            if (token.startsWith('Bearer')) {
+                token = token.slice(7, token.length).trimStart();
+            }
+            const verified = jwt.verifiy(token, process.env.JWTSECRET);
+            if (verified) {
+                next();
+            }
+            else {
+                return res.status(403).json({ errMsg: "Access denied" });
+            }
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ errMsg: "Server Error" });
+        }
+    }
+};
+//# sourceMappingURL=auth.js.map
